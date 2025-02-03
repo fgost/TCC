5f297011c0fe76d1cf167bd8b320f7fb1bb7d847
fgost <math.firmiano@gmail.com>
2025-02-02 23:54:29 -0300

    Remoção do liquibase deste projeto

--- src/main/resources/db/initialize/initialize_categories_cars.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO categories_cars (category_id, car_id)
-SELECT c.id, t.id
-FROM categories c
-CROSS JOIN cars t;
--- src/main/resources/db/initialize/initialize_categories_types.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO categories_types (category_id, type_id)
-SELECT c.id, t.id
-FROM categories c
-CROSS JOIN types t;
